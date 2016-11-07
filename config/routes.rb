04321ab934942b261e9dc04ad44adb29a04f905a
Gina <gdebell@gmail.com>
2016-11-07 14:08:16 -0700

    Passing tests for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
-# Rails.application.routes.draw do
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   #Api definition
-#   namespace :api, defaults: { format: :json} do
-#     #We are going to list our resources here
-#   end
-# end
+require 'api_constraints'
 
-Rails.application.routes.draw do
+CityGemApi::Application.routes.draw do
+  # Api definition
   namespace :api, defaults: { format: :json },
-  constraints: { subdomain: 'api' }, path: '/' do
-  #We are going to list our resources here
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
