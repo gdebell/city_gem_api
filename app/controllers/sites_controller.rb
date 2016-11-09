26c18ec014d9e377c2924212d3513895d440715b
Gina <gdebell@gmail.com>
2016-11-09 10:05:58 -0700

    working routes include: get all, get 1, delete 1, create 1 new post

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,58 +1,53 @@
-class SitesController < ApplicationController
-  before_action :set_site, only: [:show, :edit, :update, :destroy]
+# serve up json in controller
 
+class SitesController < ApplicationController
+
+  #WORKING  THIS WILL GET ALL POSTS
   # GET /sites
   def index
     @sites = Site.all
+    render json: @sites
   end
 
+  #WORKING THIS WILL SHOW ONE POST BASED ON PARAMS
   # GET /sites/1
   def show
+    @site = Site.find(params[:id])
+    render json: @site
   end
 
-  # GET /sites/new
-  def new
-    @site = Site.new
-  end
-
-  # GET /sites/1/edit
-  def edit
-  end
-
-  # POST /sites
-  def create
-    @site = Site.new(site_params)
-
-    if @site.save
-      redirect_to @site, notice: 'Site was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /sites/1
-  def update
-    if @site.update(site_params)
-      redirect_to @site, notice: 'Site was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /sites/1
+  #WORKING THIS WILL DELETE A POST
   def destroy
-    @site.destroy
-    redirect_to sites_url, notice: 'Site was successfully destroyed.'
+    Site.destroy(params[:id])
+  end
+
+  #WORKING
+  def create
+    @site = Site.create(site_params)
+    @site.save
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_site
-      @site = Site.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def site_params
-      params.require(:site).permit(:name, :description, :city, :state, :photo, :lat, :long)
-    end
+  def site_params
+    params.require(:site).permit(:name, :description, :city, :state, :lat, :long, :photo)
+  end
+
+######
+
+  # GET /sites/1/edit
+  # def edit
+  # end
+  #
+  # # PATCH/PUT /sites/1
+  # def update
+  #   if @site.update(site_params)
+  #     redirect_to @site, notice: 'Site was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 require 'api_constraints'
 
 CityGemApi::Application.routes.draw do
-  devise_for :sites
-  # Api definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
-      resources :sites, :only => [:show]
-    end
-  end
+  resources :sites
+  root 'sites#index'
 end
